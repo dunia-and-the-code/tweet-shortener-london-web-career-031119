5bceb1ebe12ea5093fdcc5564b814679885240e7
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-14 00:31:00 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def dictionary
+  words_to_be_substituted = {
+    "hello" => "hi",
+    "to, two, too" => "2",
+    "for, four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+
+end
+
+def word_substituter(string)
+  dictionary_a = words_to_be_substituted.keys
+  string_a = string.split(//)
+    string_a.each do |word|
+      if word == dictionary_a
+  
+end
